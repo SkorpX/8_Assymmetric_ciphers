aa4d4419c5201816d795f562686e90c1effb3710
Skorpx <sdn2000@bk.ru>
2019-12-27 09:36:50 +0300

    general task

--- client.py
+++ client.py
@@ -1,14 +1,40 @@
 import socket
 import pickle
 
+def secret_key(g,b,p):
+    return g ** b % p
+
+def ecrypt(msg, k):
+    string=''
+    for i in range(len(msg)):
+        string+=chr(ord(msg[i])^k)
+    return string
+
+def send_msg(sock, msg, k):
+    sock.send(pickle.dumps(ecrypt(msg, k)))
+
+def recv_msg(sock, k):
+	msg=ecrypt(pickle.loads(sock.recv(1024)), k)
+	return msg
+
 HOST = '127.0.0.1'
 PORT = 8080
-
 sock = socket.socket()
 sock.connect((HOST, PORT))
+print('Success!')
+print('Write "exit" to disconnect')	
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
-
-sock.close()
+p, g, b = 3,5,7  
+A = secret_key(g,b,p)
+sock.send(pickle.dumps((g, A, p)))
+B = pickle.loads(sock.recv(1024))
+K = secret_key(B,b,p)
+print('Secret key:',K)
+while True: 
+    msg = input('Your message: ')
+    if msg !='exit':
+        send_msg(sock, msg, K)
+        print(recv_msg(sock, K))
+    else:
+        break
+sock.close() 
--- server.py
+++ server.py
@@ -1,15 +1,43 @@
 import socket
 import pickle
+from random import randint
 
 HOST = '127.0.0.1'
 PORT = 8080
+def secret_key(g,a,p):
+    return g**a%p
+def ecrypt(msg, key):
+    string = ''
+    for i in range(len(msg)):
+        string +=chr(ord(msg[i])^key)
+    return string
+def send_msg(conn, msg, key):
+    conn.send(pickle.dumps(ecrypt(msg, key)))
+def recv_msg(conn, key):
+	msg= ecrypt(pickle.loads(conn.recv(1024)), key)
+	return msg
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
+sock = socket.socket()        
+sock.bind((HOST,PORT))
+print('Your port:',PORT)
+sock.listen(0)			
+sock.setblocking(1)
 conn, addr = sock.accept()
 
+b = randint(1, 32)
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+g, A, p = pickle.loads(msg)
 
-conn.close()
+B = secret_key(g,b,p)
+conn.send(pickle.dumps(B))
+
+K = secret_key(A,b,p)
+print('Secret key:',K)
+while True:
+	try:
+		msg = recv_msg(conn, K)
+		print(msg)
+		send_msg(conn, 'Success', K)
+	except:
+		break
+conn.close() 
